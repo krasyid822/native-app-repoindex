6922112e3cc6feb0dad9585836133508efdafd1c
krasyid822 <krasyid822@gmail.com>
2025-08-25 09:17:47 +0700

    Update app icons: replace outdated icon URLs with new ones for better accessibility and consistency.

--- data.js
+++ data.js
@@ -6,7 +6,7 @@ const appList = [
         kategori: "Alat",
         deskripsiSingkat: "Kunci jaringan 4G LTE untuk koneksi internet yang lebih stabil.",
         deskripsiLengkap: "Dengan Aplikasi 4G Lock, Anda dapat mengunci jaringan 4G LTE pada perangkat Anda untuk memastikan koneksi internet yang lebih stabil dan cepat. Sangat cocok untuk streaming, gaming, dan browsing tanpa gangguan.",
-        ikon: "https://github.com/krasyid822/lte_lock/blob/dc758b917d25808ac74cb50fc8a487eff559a465/web/icons/Icon-maskable-512.png?raw=true",
+        ikon: "https://github.com/krasyid822/lte_lock/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
         screenshots: [
             "https://github.com/krasyid822/lte_lock/blob/e0d1a0e195efce6e76dd3f03b409c5cdac17b5d4/SCREENSHOTS/Screenshot_2025-08-24-21-29-40-69_426a5588c5110cd01d5af17e47adb223.jpg?raw=true",
             "https://github.com/krasyid822/lte_lock/blob/e0d1a0e195efce6e76dd3f03b409c5cdac17b5d4/SCREENSHOTS/Screenshot_2025-08-24-21-27-50-77_37a66d687910f92b4549cb2321250240.jpg?raw=true",
@@ -23,7 +23,7 @@ const appList = [
         kategori: "Alat",
         deskripsiSingkat: "Senter hantu yang dapat menyala dalam gelap.",
         deskripsiLengkap: "Senter Hantu adalah aplikasi yang dirancang untuk memberikan penerangan dalam kegelapan. Dengan fitur-fitur canggih, aplikasi ini dapat membantu Anda menemukan jalan di tempat yang gelap.",
-        ikon: "https://github.com/krasyid822/senter_hantu/blob/c021e6d00d24920316d5c0909642178cc97defee/web/icons/Icon-maskable-512.png?raw=true",
+        ikon: "https://github.com/krasyid822/senter_hantu/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
         screenshots: [
             "https://github.com/krasyid822/senter_hantu/blob/main/SCREENSHOTS/Screenshot_2025-08-24-21-48-13-44_8bf4a8e107b8116395c0f1cdbaa7596a.jpg?raw=true",
             "https://github.com/krasyid822/senter_hantu/blob/main/SCREENSHOTS/Screenshot_2025-08-24-21-47-45-35_8bf4a8e107b8116395c0f1cdbaa7596a.jpg?raw=true",
@@ -39,7 +39,7 @@ const appList = [
         kategori: "Produktivitas",
         deskripsiSingkat: "Jadwal sholat yang akurat dan mudah digunakan.",
         deskripsiLengkap: "Aplikasi ini menyediakan jadwal sholat yang tepat waktu dan notifikasi untuk setiap waktu sholat (dalam pengembangan). Dengan antarmuka yang sederhana, pengguna dapat dengan mudah menavigasi dan menemukan informasi yang mereka butuhkan.",
-        ikon: "https://github.com/krasyid822/jadwal_sholat_app/blob/main/web/icons/Icon-maskable-512.png?raw=true",
+        ikon: "https://github.com/krasyid822/jadwal_sholat_app/blob/main/android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png?raw=true",
         screenshots: [
             "https://github.com/krasyid822/jadwal_sholat_app/blob/main/SCREENSHOTS/Screenshot_2025-08-24-21-59-29-20_c07d3fa2d26ebcd173936b391c9ec37b.jpg?raw=true",
             "https://github.com/krasyid822/jadwal_sholat_app/blob/main/SCREENSHOTS/Screenshot_2025-08-24-21-59-13-52_c07d3fa2d26ebcd173936b391c9ec37b.jpg?raw=true",
